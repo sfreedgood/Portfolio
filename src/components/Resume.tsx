c8dae5dcc5267cba64ed57e2b5126fd75c68cef8
Sam Freedgood <sam@samfreedgood.dev>
2024-10-02 17:31:31 +0200

    add resume image and fullscreen popover onclick

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,33 +1,33 @@
-import Icon from "./Icon";
-import externalLinkIcon from '../assets/external-link.png'
+import { useState } from "react";
+import { Popover } from "@mui/material";
 import ResponsiveAppBar from "./ResponsiveAppBar";
-// import { NavBar } from "./ResponsiveAppBar";
 
 export function Resume() {
+  const [showModal, setShowModal] = useState<boolean>(false);
   return (
     <>
       <ResponsiveAppBar />
-      <div style={{
+      <div
+        style={{
           display: "flex",
-          minWidth: "80vw",
-          minHeight: "80vh",
-          height: "fit-content",
+          justifyContent: "center",
+          height: "85vh",
           paddingBottom: 0,
-          boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
-          marginTop: "1.6em",
-          marginBottom: "0.9em",
-          overflow: "hidden",
-          borderRadius: "8px",
-          willChange: "transform"
-      }}>
-        <iframe loading="lazy" style={{
-          width: "100%",
-          border: "none",
+          margin: "1em 0 1em 0",
         }}
-          src="https://www.canva.com/design/DAF7BhkOGP0/wzuBuT9HyXwZDjbcfUDGyA/view?embed" allowFullScreen={true} allow="fullscreen">
-        </iframe>
+      >
+        <img
+          onClick={() => setShowModal(true)}
+          src="/Sam Freedgood - Software Engineer.png"
+          alt="CV image"
+        />
       </div>
-      <Icon icon={externalLinkIcon} url="https://www.canva.com/design/DAF7BhkOGP0/wzuBuT9HyXwZDjbcfUDGyA/view?utm_content=DAF7BhkOGP0&utm_campaign=designshare&utm_medium=embeds&utm_source=link" altText="External Link" text="View Resume on Canva" />
+      <Popover open={showModal} onClick={() => setShowModal(false)}>
+        <img
+          src="/Sam Freedgood - Software Engineer.png"
+          alt="Fullscreen CV image"
+        />
+      </Popover>
     </>
   );
 }
